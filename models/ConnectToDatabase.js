64433e8efc61aa7003934c2b62d88af4f75b076b
Mupsy <benjibou02@gmail.com>
2025-01-17 12:15:54 +0100

    fix connect to database

--- models/ConnectToDatabase.js
+++ models/ConnectToDatabase.js
@@ -3,13 +3,9 @@ require("dotenv").config();
 
 var con = mariadb.createPool({
     host: "100.92.95.45",
-<<<<<<< HEAD
+
     user: process.env.USER_BDD,
     password: process.env.USER_BDD_PW,
-=======
-    user: '',
-    password: "",
->>>>>>> d85cb8a9e8ce6f6c5fdef30e418623cf12d8c156
     database:"Noxus_Arena",
     port: 3306,
     connectionLimit: 50,  // Augmenter le nombre de connexions dans le pool
