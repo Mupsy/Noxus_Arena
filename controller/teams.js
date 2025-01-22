7660599f511551af5c142b589161ea07812e1354
Ass4sin <tudorandrei2010@yahoo.com>
2025-01-22 22:38:46 +0100

    aled

--- controller/teams.js
+++ controller/teams.js
@@ -3,11 +3,32 @@ const axios = require("axios");
 require("dotenv").config();
 
 module.exports = async (req, res) => {
+    connection = await con.getConnection();
+    const getTeamInfo = "SELECT Team_Name, Player_Role, Player_Champion FROM Team_Info";
+
+    const rows = await connection.execute(getTeamInfo);
+
+    res.json({
+        success: true,
+        message: "team info",
+        data:rows,
+    });
+
     try {
+        const insertTeamInfo = "INSERT INTO Team_Info (Team_Name, Player_Role, Player_Champion) VALUES (?, ?, ?)";
+
         const { teamName, playerRole, champion } = req.body;
-        console.log("Received data:", req.body);
-        // Insert database logic or other processing here
-        res.json({ success: true, message: "Team created successfully!" });
+        //connect to db
+        console.log("Connected to Database:");
+
+        //do team info query
+        await connection.execute(insertTeamInfo, [teamName, playerRole, champion]);
+        console.log("Team info added to db")
+
+        //show information on the page
+
+
+        // res.json({ success: true, message: "Team created successfully!" });
     } catch (error) {
         console.error("Error in teamController:", error);
         res.status(500).json({ success: false, message: "Error creating team." });
